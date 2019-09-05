092564eff9806106e179f03a437dfb74608cf79d
Samuel <samuelpedro1999@gmail.com>
2019-09-04 22:17:09 -0300

    Ex7 incompleto

--- Ex7/src/ex7/Ex7.java
+++ Ex7/src/ex7/Ex7.java
@@ -16,6 +16,42 @@ public class Ex7 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Entre com a hora:");
+        int hora1 = sc.nextInt();
+        System.out.print("Entre com os minutos:");
+        int minuto1 = sc.nextInt();
+        System.out.print("Entre com os segundos:");
+        int segundo1 = sc.nextInt();
+        System.out.print("Entre com a segunda hora:");
+        int hora2 = sc.nextInt();
+        System.out.print("Entre com os minutos:");
+        int minuto2 = sc.nextInt();
+        System.out.print("Entre com os segundos:");
+        int segundo2 = sc.nextInt();
+        int horario1 = segundo1 + (minuto1*60) + (hora1*3600);
+        int horario2 = segundo2 + (minuto2*60) + (hora2*3600);
+        int horario3,hora3,minuto3,segundo3;
+        if(horario1 > horario2){
+            horario3 = horario1-horario2;
+            hora3 = horario3/3600;
+            horario3 = horario3 - hora3;
+            minuto3 = horario3/60;
+            horario3 = horario3 - minuto3;
+            segundo3 = horario3;
+            System.out.println("A diferença de horario é: "+hora3+" : "+minuto3+" : "+segundo3);
+        }else if(horario2 > horario1){
+            horario3 = horario2-horario1;
+            hora3 = horario3/3600;
+            horario3 = horario3 - hora3;
+            minuto3 = horario3/60;
+            horario3 = horario3 - minuto3;
+            segundo3 = horario3;
+            System.out.println("A diferença de horario é: "+hora3+" : "+minuto3+" : "+segundo3);
+        }else{
+            System.out.println("A diferença de horario é: 0 : 0 : 0");
+        }
+
     }
-    
+
 }
