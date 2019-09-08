5e04df740735987abb4e72965dea059411e98713
Samuel <samuelpedro1999@gmail.com>
2019-09-08 15:47:37 -0300

    Exercicios finalizados.

--- Ex13/src/ex13/Ex13.java
+++ Ex13/src/ex13/Ex13.java
@@ -18,31 +18,61 @@ public class Ex13 {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner sc = new Scanner(System.in);
- 
         System.out.print("Informe o nome de arquivo texto:");
-        String nome = sc.nextLine();
- 
-        System.out.print("\nConteúdo do arquivo texto:\n");
+        String Nome_arq = sc.nextLine();
+        int num_mulheres=0,faixa_idade=0,quant_H=0,idade_maior=0;
+        String Paciente_velho = new String(),Mulher_Baixa = new String();
+        int quant=0,idade,peso;
+        float tamanho,tamanho_M_baixa=100;
+        float soma_idades_H=0,media_idade_H;
+        String partes[] = new String[5];
+        String nome,sexo;
         try {
-            FileReader arq = new FileReader(nome);
-            BufferedReader lerArq = new BufferedReader(arq);
- 
-            String linha = lerArq.readLine(); // lê a primeira linha
-    // a variável "linha" recebe o valor "null" quando o processo
-    // de repetição atingir o final do arquivo texto
-        while (linha != null) {
-            System.out.printf("%s\n", linha);
-        
-             linha = lerArq.readLine(); // lê da segunda até a última linha
+            FileReader file = new FileReader(Nome_arq);
+            BufferedReader lerArq = new BufferedReader(file);
+            String Line = lerArq.readLine(); // le a primeira linha
+    
+        while (Line != null) {
+            quant++;
+            partes = Line.split(";");        
+            sexo = partes[0];
+            idade =Integer.parseInt(partes[1]);
+            tamanho =Float.parseFloat(partes[2]);
+            peso = Integer.parseInt(partes[3]);
+            nome = partes[4];
+            if(sexo.equals("H")){
+                soma_idades_H = soma_idades_H + idade;
+                quant_H++;
+            }
+            else if(sexo.equals("M") && tamanho<=1.7 && tamanho>=1.6 && peso>70){
+                num_mulheres++;
+            }
+            
+            if(idade >= 18 && idade <= 25 ){
+                faixa_idade++;
+            }
+            if(idade>idade_maior){
+                idade_maior = idade;
+                Paciente_velho = nome;
+            }
+            if(sexo.equals(("M")) && tamanho<tamanho_M_baixa){
+                tamanho_M_baixa = tamanho;
+                Mulher_Baixa = nome;
+            }
+            Line = lerArq.readLine(); // le da segunda até a última linha
          }
- 
-         arq.close();
+         file.close();
         }catch (IOException e) {
-            System.err.printf("Erro na abertura do arquivo: %s.\n",
-            e.getMessage());
+            System.err.printf("Erro na abertura do arquivo: %s.\n",e.getMessage()); //exibe erro caso haja problema com leitura de arquivo.
     }
- 
-    System.out.println();
+        media_idade_H = soma_idades_H/quant_H;
+        System.out.println("Quantidade de pacientes: "+quant);
+        System.out.println("Media da idade dos homens: "+media_idade_H+quant_H);
+        System.out.println("Numero de mulheres com altura entre 1,60 e 1,70 e peso acima de 70kg: "+num_mulheres);
+        System.out.println("Quantidade de pessoas com idade entre 18 e 25: "+faixa_idade);
+        System.out.println("Nome do paciente mais velho: "+Paciente_velho);
+        System.out.println("Nome da mulher mais baixa: "+Mulher_Baixa);
+        sc.close();
     }
     
 }
