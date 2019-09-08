72f63a2ab212670c32237a6e402aa408280619e8
Samuel <samuelpedro1999@gmail.com>
2019-09-08 15:19:51 -0300

    Ex10 pronto.

--- Ex10/src/ex10/Ex10.java
+++ Ex10/src/ex10/Ex10.java
@@ -4,7 +4,10 @@
  * and open the template in the editor.
  */
 package ex10;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Collections;
+import java.util.List;
 /**
  *
  * @author grobs
@@ -17,11 +20,21 @@ public class Ex10 {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner sc = new Scanner(System.in);
-        String linha;
-        do{
-        linha = sc.nextLine();
-        
-        }while(!linha.equals("fim"));
+        List<Integer> Vetor = new ArrayList<Integer>();
+        while(true){
+            System.out.print("Entre com um inteiro:");
+            char b = sc.next().charAt(0);
+            if(b != 'f'){
+                 Vetor.add(Character.getNumericValue(b));
+             }
+             else{
+                 break;
+            }
+        }
+        System.out.println("Lista de numeros antes de ordenar:"+Vetor);
+        Collections.sort(Vetor);
+        System.out.println("Lista de numeros depois de ordenar:"+Vetor);
+        sc.close();
     }
     
 }
